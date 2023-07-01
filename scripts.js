2731e8996783bed1312af6b1ab71e0834595bba8
Joshua Leon <119650191+DevJLeon@users.noreply.github.com>
2023-06-30 22:02:14 -0500

    third commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,39 @@
-const $magnitudes = document.querySelector(".magnitudes");
-const $unitSelector = document.querySelector(".unitSelector");
+const $magnitudesDiv = document.querySelector(".magnitudes");
+const $unitSelector = document.querySelector("#unitSelector");
+console.log($unitSelector)
 
-if $unitSelector.value == ""
+let magnitudes = document.createElement("select")
+
+
+$unitSelector.addEventListener("input",(e)=>{
+    console.log(e.target.value);
+    if((e.target.value)=="temperature"){
+        magnitudes.innerHTML= `
+            <option value="celcius">Celcius</option>
+            <option value="kelvin">Kelvin</option>
+            <option value="farenheight">Farenheight</option>
+        `
+    } else if((e.target.value)=="lenght"){
+        magnitudes.innerHTML= `
+            <option value="kilometer">Kilometer</option>
+            <option value="hectometer">Hectometer</option>
+            <option value="decemetre">Decemetre</option>
+            <option value="meter">Meter</option>
+            <option value="decimeter">Decimeter</option>
+            <option value="centimeter">Centimeter</option>
+            <option value="milimeter">Milimeter</option>
+        `
+    }else if((e.target.value)=="weight"){
+        magnitudes.innerHTML= `
+            <option value="ton">Ton</option>
+            <option value="kilogram">Kilogram</option>
+            <option value="decemetre">Decemetre</option>
+            <option value="meter">Meter</option>
+            <option value="decimeter">Decimeter</option>
+            <option value="centimeter">Centimeter</option>
+            <option value="milimeter">Milimeter</option>
+        `
+    };
+    $magnitudesDiv.appendChild(magnitudes)
+})
+

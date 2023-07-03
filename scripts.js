1462214ace84ce6906ddc05cee5ad216adaf7d4b
Joshua <celisjoshleon@gmail.com>
2023-07-03 15:21:44 -0500

    selects

--- scripts.js
+++ scripts.js
@@ -1,39 +1,56 @@
 const $magnitudesDiv = document.querySelector(".magnitudes");
 const $unitSelector = document.querySelector("#unitSelector");
+const $quantityInput = document.querySelector("#quantityInput");
+const $resultInput = document.querySelector("#resultInput");
 console.log($unitSelector)
 
-let magnitudes = document.createElement("select")
+let inputMagnitude = document.createElement("select");
+let outputMagnitude = document.createElement("select");
+let toDiv =document.createElement("div");
 
 
 $unitSelector.addEventListener("input",(e)=>{
     console.log(e.target.value);
     if((e.target.value)=="temperature"){
-        magnitudes.innerHTML= `
-            <option value="celcius">Celcius</option>
+        $quantityInput.disabled = false;
+        $resultInput.disabled = false;
+        outputMagnitude.innerHTML = inputMagnitude.innerHTML= `
+            <option value="celcius" selected>Celcius</option>
             <option value="kelvin">Kelvin</option>
             <option value="farenheight">Farenheight</option>
         `
     } else if((e.target.value)=="lenght"){
-        magnitudes.innerHTML= `
+        $quantityInput.disabled = false;
+        $resultInput.disabled = false;
+        outputMagnitude.innerHTML = inputMagnitude.innerHTML= `
             <option value="kilometer">Kilometer</option>
             <option value="hectometer">Hectometer</option>
             <option value="decemetre">Decemetre</option>
-            <option value="meter">Meter</option>
+            <option value="meter" selected>Meter</option>
             <option value="decimeter">Decimeter</option>
             <option value="centimeter">Centimeter</option>
             <option value="milimeter">Milimeter</option>
         `
     }else if((e.target.value)=="weight"){
-        magnitudes.innerHTML= `
+        $quantityInput.disabled = false;
+        $resultInput.disabled = false;
+        outputMagnitude.innerHTML = inputMagnitude.innerHTML= `
             <option value="ton">Ton</option>
             <option value="kilogram">Kilogram</option>
-            <option value="decemetre">Decemetre</option>
-            <option value="meter">Meter</option>
-            <option value="decimeter">Decimeter</option>
-            <option value="centimeter">Centimeter</option>
-            <option value="milimeter">Milimeter</option>
+            <option value="gram" selected>Gram</option>
+            <option value="decigram">Dedicram</option>
+            <option value="centigram">Centigram</option>
+            <option value="miligram">Miligram</option>
         `
     };
-    $magnitudesDiv.appendChild(magnitudes)
-})
 
+    inputMagnitude.setAttribute("class","col inputSelect");
+    outputMagnitude.setAttribute("class","col outputSelect");
+    toDiv.setAttribute("class","col toDiv");
+
+    toDiv.innerHTML="to"
+
+    $magnitudesDiv.appendChild(inputMagnitude);
+    $magnitudesDiv.appendChild(toDiv);
+    $magnitudesDiv.appendChild(outputMagnitude);
+})
